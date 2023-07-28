3ba4f6183905fdcc19337df04a3523a4b7787366
george.silviu <costache.georgesilviu@yahoo.ro>
2023-07-28 20:03:01 +0300

    finished home page - starting API implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import ParticlesBg from "particles-bg";
+
 function App() {
   return (
     <div className="App">
+      <ParticlesBg type="lines" bg={true} num={2000} />
       <div
         className="topContainer"
         style={{ display: "flex", justifyContent: "space-between" }}
@@ -11,9 +16,9 @@ function App() {
         <Logo />
         <Navigation />
       </div>
-
-      {/* 
+      <Rank />
       <ImageLinkForm />
+      {/* 
       <FaceRecognition /> */}
     </div>
   );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navigation = () => {
   return (
     <nav>
-      <p className="f3 mr4 link dim black underline pa3 pointer">Sign out</p>
+      <p className="f3 mr4 link dim white underline pa3 pointer ">Sign out</p>
     </nav>
   );
 };
